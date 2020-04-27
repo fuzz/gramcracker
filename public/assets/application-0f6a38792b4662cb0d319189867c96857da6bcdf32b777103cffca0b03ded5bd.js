48beae3cd02ed95007600af9b93af479727bd785
Fuzz Leonard <fuzz@kt-22.com>
2020-04-27 05:03:03 -0700

    Fix assets?

--- public/assets/application-0f6a38792b4662cb0d319189867c96857da6bcdf32b777103cffca0b03ded5bd.js
+++ public/assets/application-0f6a38792b4662cb0d319189867c96857da6bcdf32b777103cffca0b03ded5bd.js
@@ -16340,12 +16340,199 @@ Released under the MIT license
 
 
 
+
+/*
+jQuery Infinite Pages v0.2.3
+https://github.com/magoosh/jquery-infinite-pages
+
+Released under the MIT License
+ */
+
+(function() {
+  var slice = [].slice;
+
+  (function($, window) {
+    var InfinitePages;
+    InfinitePages = (function() {
+      InfinitePages.prototype.defaults = {
+        debug: false,
+        navSelector: 'a[rel=next]',
+        buffer: 1000,
+        debounce: 250,
+        loading: null,
+        success: null,
+        error: null,
+        context: window,
+        state: {
+          paused: false,
+          loading: false
+        }
+      };
+
+      function InfinitePages(container, options) {
+        this.options = $.extend({}, this.defaults, options);
+        this.$container = $(container);
+        this.$context = $(this.options.context);
+        this.init();
+      }
+
+      InfinitePages.prototype.init = function() {
+        var debounce, scrollHandler, scrollTimeout;
+        scrollTimeout = null;
+        scrollHandler = ((function(_this) {
+          return function() {
+            return _this.check();
+          };
+        })(this));
+        debounce = this.options.debounce;
+        return this.$context.on('scroll.infinitePages', function() {
+          if (scrollTimeout && self.active) {
+            clearTimeout(scrollTimeout);
+            scrollTimeout = null;
+          }
+          return scrollTimeout = setTimeout(scrollHandler, debounce);
+        });
+      };
+
+      InfinitePages.prototype._log = function(msg) {
+        if (this.options.debug) {
+          return typeof console !== "undefined" && console !== null ? console.log(msg) : void 0;
+        }
+      };
+
+      InfinitePages.prototype.check = function() {
+        var distance, nav, windowBottom;
+        nav = this.$container.find(this.options.navSelector);
+        if (nav.length === 0) {
+          return this._log("No more pages to load");
+        } else {
+          windowBottom = this.$context.scrollTop() + this.$context.height();
+          distance = nav.offset().top - windowBottom;
+          if (this.options.state.paused) {
+            return this._log("Paused");
+          } else if (this.options.state.loading) {
+            return this._log("Waiting...");
+          } else if (distance > this.options.buffer) {
+            return this._log((distance - this.options.buffer) + "px remaining...");
+          } else {
+            return this.next();
+          }
+        }
+      };
+
+      InfinitePages.prototype.next = function() {
+        if (this.options.state.done) {
+          return this._log("Loaded all pages");
+        } else {
+          this._loading();
+          return this.jqXHR = $.getScript(this.$container.find(this.options.navSelector).attr('href')).done((function(_this) {
+            return function() {
+              return _this._success();
+            };
+          })(this)).fail((function(_this) {
+            return function() {
+              return _this._error();
+            };
+          })(this));
+        }
+      };
+
+      InfinitePages.prototype._loading = function() {
+        this.options.state.loading = true;
+        this._log("Loading next page...");
+        if (typeof this.options.loading === 'function') {
+          return this.$container.find(this.options.navSelector).each(this.options.loading);
+        }
+      };
+
+      InfinitePages.prototype._success = function() {
+        this.options.state.loading = false;
+        this.jqXHR = null;
+        this._log("New page loaded!");
+        if (typeof this.options.success === 'function') {
+          return this.$container.find(this.options.navSelector).each(this.options.success);
+        }
+      };
+
+      InfinitePages.prototype._error = function() {
+        this.options.state.loading = false;
+        this._log("Error loading new page :(");
+        if (typeof this.options.error === 'function') {
+          return this.$container.find(this.options.navSelector).each(this.options.error);
+        }
+      };
+
+      InfinitePages.prototype.pause = function() {
+        this.options.state.paused = true;
+        return this._log("Scroll checks paused");
+      };
+
+      InfinitePages.prototype.resume = function() {
+        this.options.state.paused = false;
+        this._log("Scroll checks resumed");
+        return this.check();
+      };
+
+      InfinitePages.prototype.stop = function() {
+        this.$context.off('scroll.infinitePages');
+        return this._log("Scroll checks stopped");
+      };
+
+      InfinitePages.prototype.abort = function() {
+        if (this.jqXHR) {
+          this.jqXHR.abort();
+          this.jqXHR = null;
+          return this._log("Page load aborted!");
+        } else {
+          return this._log("There was no request to abort");
+        }
+      };
+
+      return InfinitePages;
+
+    })();
+    return $.fn.extend({
+      infinitePages: function() {
+        var args, option;
+        option = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
+        return this.each(function() {
+          var $this, data;
+          $this = $(this);
+          data = $this.data('infinitepages');
+          if (!data) {
+            $this.data('infinitepages', (data = new InfinitePages(this, option)));
+          }
+          if (typeof option === 'string') {
+            if (option === 'destroy') {
+              return data.stop(args);
+            } else if (option === 'reinit') {
+              return data.init(args);
+            } else {
+              return data[option].apply(data, args);
+            }
+          }
+        });
+      }
+    });
+  })(window.jQuery, window);
+
+}).call(this);
 (function() {
 
 
 }).call(this);
 (function() {
-
+  $(function() {
+    return $('.pagination').infinitePages({
+      debug: true,
+      loading: function() {
+        return $(this).text('Loading next page...');
+      },
+      error: function() {
+        return $(this).button('There was an error, please try again');
+      }
+    });
+  });
 
 }).call(this);
 // This is a manifest file that'll be compiled into application.js, which will include all the files
@@ -16360,6 +16547,7 @@ Released under the MIT license
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 
 
 
