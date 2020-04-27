197bec8b490ad87f5818af2b5838aa3fc3962a53
Fuzz Leonard <fuzz@kt-22.com>
2020-04-26 18:11:15 -0700

    Move paginates_per

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post < ApplicationRecord
   include Contestable
 
-  paginates_per 60
-
   belongs_to :user
   has_one_attached :image
   has_many :email_signups
   has_many :likes
+
+  paginates_per 60
 
   # Allow simple_form to use params[:post][:email] for email signups
   attr_accessor :email  
